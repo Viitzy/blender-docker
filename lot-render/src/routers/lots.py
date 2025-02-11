b6cec0b909e47fe5245549663801c34f897b6736
Viitzy <67331247+Viitzy@users.noreply.github.com>
2025-02-11 17:53:52 -0300

    changing endpoints adding mongo

--- lot-render/src/routers/lots.py
+++ lot-render/src/routers/lots.py
@@ -66,7 +66,6 @@ async def detect_lot(request: DetectLotRequest):
         zoom=request.zoom,
         confidence=request.confidence,
         object_id=request.object_id,
-        street_name=request.street_name,
         year=request.year,
     )
 
